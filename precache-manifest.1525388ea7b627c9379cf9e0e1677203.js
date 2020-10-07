31e4dde8cd1be088334c22611d6f0edee85ddc09
Austin <austinknauer@alumni.colostate.edu>
2020-10-06 21:42:29 -0600

    Updates

---
+++ precache-manifest.1525388ea7b627c9379cf9e0e1677203.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bd3663a6657e26d534d335dc736b39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11605c0be11fab6731be",
+    "url": "/static/css/main.94473712.chunk.css"
+  },
+  {
+    "revision": "214bc39e76db0ecfae02",
+    "url": "/static/js/2.6a075f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a075f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11605c0be11fab6731be",
+    "url": "/static/js/main.93397569.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "86500fd196e8c2d1a69b848f54cd5112",
+    "url": "/static/media/Austin Knauer F.86500fd1.pdf"
+  },
+  {
+    "revision": "c449a40d5b635ea40ceb0fce665b6fac",
+    "url": "/static/media/calculator.c449a40d.png"
+  },
+  {
+    "revision": "e1f0dbb0566d87cb5fff1607e6d87a21",
+    "url": "/static/media/clock.e1f0dbb0.png"
+  },
+  {
+    "revision": "b0c3a59e504c40f00d3b49830b9f6c14",
+    "url": "/static/media/desert.b0c3a59e.gif"
+  },
+  {
+    "revision": "edc53a401519c60facc5c0e1b318ec79",
+    "url": "/static/media/drum.edc53a40.png"
+  },
+  {
+    "revision": "62801f87d054dd3b169faae4ef28de64",
+    "url": "/static/media/landing.62801f87.png"
+  },
+  {
+    "revision": "87a65d9e49d046dd91c6309364646551",
+    "url": "/static/media/markdown.87a65d9e.png"
+  },
+  {
+    "revision": "3621237d945d8e96e7dc84fb231912ac",
+    "url": "/static/media/me.3621237d.jpg"
+  },
+  {
+    "revision": "f616348b89784445a9617f7d4ad6c7b2",
+    "url": "/static/media/mountains.f616348b.gif"
+  },
+  {
+    "revision": "aaf4371d51952f536081a7e96cd1ce4b",
+    "url": "/static/media/ocean.aaf4371d.gif"
+  },
+  {
+    "revision": "8b0c3fdba18c98129ebe4c3e4c5d85b9",
+    "url": "/static/media/quote.8b0c3fdb.png"
+  },
+  {
+    "revision": "0525b54d0bb79beae2782108586330ac",
+    "url": "/static/media/static.0525b54d.gif"
+  }
+]);
